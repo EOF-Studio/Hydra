1af40bb2c908387549321e70c57e5aadbccca9ac
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2011-05-11 12:20:11 +0200

    Abstract plugin class with rudimentary functionality
    Signed-off-by: Jesper Fyhr Knudsen <jesper@fyhr.dk>

---
+++ trunk/Hydra.Commons/src/com/eofstudio/hydra/commons/plugin/APlugin.java
@@ -0,0 +1,28 @@
+package com.eofstudio.hydra.commons.plugin;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public abstract class APlugin implements IPlugin 
+{
+	private ArrayList<Socket> _ActiveConnections;
+	private int               _InstanceID;
+	
+	public APlugin( )
+	{
+		_ActiveConnections = new ArrayList<Socket>();
+	}
+	
+	@Override
+	public int getInstanceID() 
+	{
+		return _InstanceID;
+	}
+
+	@Override
+	public void AddConnection( Socket socket ) 
+	{
+		_ActiveConnections.add( socket );
+	}
+
+}
--- trunk/Hydra.Commons/src/com/eofstudio/hydra/commons/plugin/IPlugin.java
+++ trunk/Hydra.Commons/src/com/eofstudio/hydra/commons/plugin/IPlugin.java
@@ -1,7 +1,9 @@
 package com.eofstudio.hydra.commons.plugin;
 
+import java.net.Socket;
+
 public interface IPlugin 
 {
 	int getInstanceID();
-	
+	void AddConnection( Socket socket );
 }
