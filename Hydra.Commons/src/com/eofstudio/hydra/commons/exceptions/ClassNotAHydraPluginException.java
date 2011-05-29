bb181874d6d5ee7611802ec225562179a875baeb
Jesper Fyhr Knudsen <jesper@Jesper-Fyhr-Knudsens-MacBook-Pro.local>
2011-05-30 01:06:12 +0200

    Merged with master

---
+++ Hydra.Commons/src/com/eofstudio/hydra/commons/exceptions/ClassNotAHydraPluginException.java
@@ -0,0 +1,27 @@
+package com.eofstudio.hydra.commons.exceptions;
+
+public class ClassNotAHydraPluginException extends Exception 
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2882363698629336223L;
+
+	public ClassNotAHydraPluginException() {
+		super();
+	}
+
+	public ClassNotAHydraPluginException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public ClassNotAHydraPluginException(String arg0) {
+		super(arg0);
+	}
+
+	public ClassNotAHydraPluginException(Throwable arg0) {
+		super(arg0);
+	}
+	
+}
